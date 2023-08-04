22efd43f45d19fced035f572ab34665cf16cb03b
LKA102 <stefano.hernandez@unmsm.edu.pe>
2023-08-04 13:14:28 -0500

    Añadiendo cuadro de detalles

--- FrontEnd/scripts/Eventos.js
+++ FrontEnd/scripts/Eventos.js
@@ -1,11 +1,18 @@
+//Botones encontrar ruta e información
 const botonEncontrarRuta = document.getElementById("btn-encontrar-ruta")
 const botonInformacion  = document.getElementById("btn-informacion")
 const dropDowns = document.getElementById('espacio-encontrar-ruta')
+const cuadroInfo = document.getElementById('cuadroInfo');
+
 
 botonInformacion.addEventListener('click', () => {
     dropDowns.style.display = 'none'
+    cuadroInfo.style.display = 'block'
 });
 
 botonEncontrarRuta.addEventListener('click', () => {
     dropDowns.style.display = 'block'
-})
+    cuadroInfo.style.display = 'none'
+});
+
+
